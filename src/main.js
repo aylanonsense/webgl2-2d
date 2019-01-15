6af69ce039d613f228a120b9d93245e64473ef00
bridgs <bridgs.dev@gmail.com>
2019-01-15 00:48:26 -0500

    Apple

--- src/main.js
+++ src/main.js
@@ -2,19 +2,32 @@ import * as PIXI from 'pixi.js'
 import * as shader from './shader/simple-red'
 
 export default async () => {
-  const app = new PIXI.Application()
-  document.body.appendChild(app.view)
-  PIXI.loader.add('hat', 'img/wizard-hat.png').load((loader, resources) => {
-    const hat = new PIXI.Sprite(resources.hat.texture);
-    hat.x = app.renderer.width / 2
-    hat.y = app.renderer.height / 2
-    hat.anchor.x = 0.5
-    hat.anchor.y = 0.5
-
-    app.stage.addChild(hat)
-
-    app.ticker.add(() => {
-      hat.rotation += 0.01
-    })
+  const app = new PIXI.Application({
+    view: document.getElementById('canvas'),
+    width: 600,
+    height: 600,
+    roundPixels: true
   })
+  app.renderer.reset()
+
+  let apple
+
+  const update = dt => {
+    apple.rotation += 0.01 * dt
+  }
+
+  PIXI.loader
+    .add('apple', 'img/apple.png')
+    .load((loader, resources) => {
+      resources.apple.texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
+      apple = new PIXI.Sprite(resources.apple.texture);
+      apple.x = app.renderer.width / 2
+      apple.y = app.renderer.height / 2
+      apple.anchor.x = 0.5
+      apple.anchor.y = 0.5
+      apple.scale.x = 4
+      apple.scale.y = 4
+      app.stage.addChild(apple)
+      app.ticker.add(delta => update(delta))
+    })
 }
